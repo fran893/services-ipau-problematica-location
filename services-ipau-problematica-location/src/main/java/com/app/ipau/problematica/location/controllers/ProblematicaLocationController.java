732e2e028a0d628bfa0099ab0af81df2bc65930e
Franco <alvarez.fr893@gmail.com>
2020-02-20 10:29:30 -0300

    Se agrego endpoint que actualiza la cantidad de veces marcada de la prob

--- services-ipau-problematica-location/src/main/java/com/app/ipau/problematica/location/controllers/ProblematicaLocationController.java
+++ services-ipau-problematica-location/src/main/java/com/app/ipau/problematica/location/controllers/ProblematicaLocationController.java
@@ -9,8 +9,11 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 import com.app.ipau.commons.problematica.location.models.entities.ProblematicaLocation;
 import com.app.ipau.problematica.location.models.services.IProblematicaLocation;
@@ -40,8 +43,17 @@ public class ProblematicaLocationController {
 	}
 	
 	@PostMapping("/addProblematicaLocation")
+	@ResponseStatus(HttpStatus.CREATED)
 	public ProblematicaLocation saveProblematicaLocation(@RequestBody @Valid ProblematicaLocation problematicaLocaction) {
 		return probLocationServices.save(problematicaLocaction);
+	}
+	
+	@PutMapping("/updateCountMarker/{idProbLocation}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ProblematicaLocation updateProbLocation(@PathVariable Long idProbLocation) {
+		ProblematicaLocation problematicaLocationDb = probLocationServices.findById(idProb);
+		problematicaLocationDb.setCantVecesMarcada(problematicaLocationDb.getCantVecesMarcada() + 1);
+		return probLocationServices.save(problematicaLocationDb);
 	}
 	
 	@GetMapping("/pruebaProfile")
